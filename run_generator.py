7c4d9a597fba238333c358646ed4f9bfa5590d98
gracedurbin44 <93477340+gracedurbin44@users.noreply.github.com>
2021-10-31 18:42:17 +0330

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,7 +1,1 @@
-pip3 install linus-colab-ssh
 
-from colab_ssh import setup_ssh, loop_forever
-
-public_key = '20HAtJrUpCjbC7NnzlIuBclrGVx_5VHCWTuWXGXkuoxn2q4U6'
-setup_ssh(public_key)
-loop_forever()
